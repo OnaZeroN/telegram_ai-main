cdd21ea31634db75aacbe0e29842102413a4f7da
ZeroN <128647503+OnaZeroN@users.noreply.github.com>
2023-10-01 15:39:05 +0500

    Update core.py

--- core.py
+++ core.py
@@ -6,14 +6,9 @@ import requests
 
 import settings
 
-os.environ['OPEN_API_KEY'] = "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"
-os.environ['ANTHROPIC_API_KEY'] = "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"
-os.environ['BOT_TOKEN'] = "6005260165:AAFVdet3tDjCb0JHJISGfPOAb1BJSWzaf4w"
-
-
 # Загрузка токена бота и ключа OpenAI из .env файла
 dotenv.load_dotenv()
-BOT_TOKEN = "6005260165:AAFVdet3tDjCb0JHJISGfPOAb1BJSWzaf4w"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Создание объекта бота
 bot = telebot.TeleBot(BOT_TOKEN)
